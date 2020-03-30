0f0428b1f3bc75d11991dfd7db10d1c9abe3d289
MathsPrado <matheusprado55@gmail.com>
2020-03-29 22:15:52 -0300

    first commit

---
+++ EFCORE_WEBAPI_E/EFCORE_WEBAPI_E/Model/Batalha.cs
@@ -0,0 +1,17 @@
+﻿using EFCORE_WEBAPI_E.Model;
+using System;
+using System.Collections.Generic;
+
+namespace EFCORE.WEBAPI_E.Model
+{
+    public class Batalha
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DtInicio { get; set; }
+        public DateTime DtFim { get; set; }
+        public List<HeroiBatalha> HeroisBatalhas { get; set; }
+
+    }
+}
---
+++ EFCORE_WEBAPI_E/EFCORE_WEBAPI_E/Model/HeroiBatalha.cs
@@ -0,0 +1,16 @@
+﻿using EFCORE.WEBAPI_E.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCORE_WEBAPI_E.Model
+{
+    public class HeroiBatalha
+    {
+        public int HeroiId { get; set; }
+        public Heroi Heroi { get; set; }
+        public int BatalhaId { get; set; }
+        public Batalha Batalha { get; set; }
+    }
+}
